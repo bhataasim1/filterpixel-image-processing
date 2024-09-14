456dd4694888913b90ac9ace0fca7cfe74bb568d
bhataasim1 <bhataasim700@gmail.com>
2024-09-14 16:54:04 +0530

    Refactor server initialization and error handling in App.ts

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { sendApiResponseMiddleware } from "./middleware/apiResponse.middleware";
-import { BaseEnvironment } from "./Environment";
-import errorHandler from "./middleware/errorHandler.middleware";
+import App from "./App";
 
-dotenv.config();
+const server = new App();
 
-const app: Application = express();
-const env = new BaseEnvironment();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(sendApiResponseMiddleware);
-app.use(errorHandler);
-
-app.get("/health-check", (req: Request, res: Response) => {
-  return res.send("I am alive!");
-});
-
-app.listen(env.PORT, () => {
-  console.log(`Server is running on ${env.HOST}:${env.PORT}`);
-});
+server.listen();
