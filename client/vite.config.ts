6d01faab2e0b8e6b54f268ebcb24609641f13872
bhataasim1 <bhataasim700@gmail.com>
2024-09-14 20:11:17 +0530

    Add Shadcn library for styling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   },
+// })
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import tailwindcss from 'tailwindcss'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -10,4 +24,10 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
