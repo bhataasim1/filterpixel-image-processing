2f2f6a97bc5788877dbefcec79ef90b759287e2b
bhataasim1 <bhataasim700@gmail.com>
2024-09-14 18:15:29 +0530

    Add initial project files and configurations

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="flex items-center justify-center text-3xl text-red-500">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
