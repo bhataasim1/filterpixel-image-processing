efba9197de03600326670d729ef26c1058a017b3
bhataasim1 <bhataasim700@gmail.com>
2024-09-14 20:02:30 +0530

    Add ImageUpload, ImagePreview, and ImageControls components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
+import React from "react";
+import ImageUpload from "./components/ImageUpload";
+import ImagePreview from "./components/ImagePreview";
+import ImageControls from "./components/ImageControls";
+import { ImageProvider } from "./context/useImageContext";
 
-
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <h1 className="flex items-center justify-center text-3xl text-red-500">
-      Hello world!
-    </h1>
-    </>
-  )
-}
+    <ImageProvider>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Image Processing App</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <ImageUpload />
+            <ImageControls />
+          </div>
+          <ImagePreview />
+        </div>
+      </div>
+    </ImageProvider>
+  );
+};
 
-export default App
+export default App;
